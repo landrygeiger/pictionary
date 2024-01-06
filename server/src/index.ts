71691c3987fad9788fde88b5e542e455529421c5
Landry Geiger <landry.geiger@gmail.com>
2024-01-05 20:02:03 -0600

    Init socket io on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,27 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import path from "path";
+import http from "http";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+
+const io = new Server(server);
+
 app.use(
   "/",
   express.static(path.join(__dirname, "../../web-app/dist/web-app/browser/"))
 );
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("a user connected");
+});
+
+server.listen(port, () => {
   console.log(`Server started on port ${port}.`);
 });
