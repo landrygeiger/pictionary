db0e3c7b1125a7d724866929ebd3753eed8058b0
Landry Geiger <landry.geiger@gmail.com>
2024-01-08 17:10:37 -0600

    Correctly wrap buttons

--- web-app/src/app/color-selector/color-selector.component.ts
+++ web-app/src/app/color-selector/color-selector.component.ts
@@ -13,5 +13,37 @@ import { NgFor } from '@angular/common';
 export class ColorSelectorComponent {
   @Input({ required: true }) config!: CanvasConfig;
 
-  readonly colors = ['#FF0000', '#0000FF', '#00FF00', '#FFFF00', '#FF00FF'];
+  readonly colors = [
+    // Monochromatic
+    '#ffffff', // White
+    '#000000', // Black
+    '#808080', // Gray
+    '#c0c0c0', // Silver
+
+    // Primary Colors
+    '#ff0000', // Red
+    '#00ff00', // Green
+    '#0000ff', // Blue
+
+    // Secondary Colors
+    '#ffff00', // Yellow
+    '#ff00ff', // Magenta
+    '#00ffff', // Cyan
+
+    // Tertiary Colors
+    '#800000', // Maroon
+    '#008000', // Olive
+    '#000080', // Navy
+
+    // Other Colors
+    '#ff9900', // Orange
+    '#cc6600', // Brown
+    '#800080', // Purple
+    '#008080', // Teal
+
+    // Additional Colors
+    '#3366cc', // Royal Blue
+    '#993366', // Rose
+    '#669966', // Mint Green
+  ];
 }
