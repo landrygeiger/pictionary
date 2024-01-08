bee7abcf1b0bf80c0d140f58cae19fefeb5221e1
Landry Geiger <landry.geiger@gmail.com>
2024-01-08 16:22:32 -0600

    Implement basic color selector

--- web-app/src/app/color-selector/color-selector.component.ts
+++ web-app/src/app/color-selector/color-selector.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { CanvasConfig } from '@pictionary/shared';
+import { ColorSelectorButtonComponent } from '../color-selector-button/color-selector-button.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-color-selector',
   standalone: true,
-  imports: [],
+  imports: [ColorSelectorButtonComponent, NgFor],
   templateUrl: './color-selector.component.html',
   styleUrl: './color-selector.component.css',
 })
 export class ColorSelectorComponent {
   @Input({ required: true }) config!: CanvasConfig;
+
+  readonly colors = [
+    'red',
+    'blue',
+    'green',
+    'yellow',
+    'purple',
+    'pink',
+    'magenta',
+  ];
 }
