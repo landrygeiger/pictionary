b56905c315637f8ee09cbc5438791bef2a0e5d7e
Landry Geiger <landry.geiger@gmail.com>
2024-01-16 12:47:47 -0600

    Fix unit tests

--- shared/test/session/removePlayerKeepListOwned.test.ts
+++ shared/test/session/removePlayerKeepListOwned.test.ts
@@ -6,23 +6,73 @@ describe("removePlayerKeepListOwned", () => {
       name: "Test Player 4",
       socketId: "test-socket-id-4",
       owner: false,
+      guessedWord: false,
+      score: 0,
     };
 
     const ps: Player[] = [
-      { name: "Test Player 1", socketId: "test-socket-id-1", owner: false },
-      { name: "Test Player 2", socketId: "test-socket-id-2", owner: true },
-      { name: "Test Player 3", socketId: "test-socket-id-3", owner: false },
+      {
+        name: "Test Player 1",
+        socketId: "test-socket-id-1",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 2",
+        socketId: "test-socket-id-2",
+        owner: true,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 3",
+        socketId: "test-socket-id-3",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
       p,
-      { name: "Test Player 5", socketId: "test-socket-id-5", owner: false },
+      {
+        name: "Test Player 5",
+        socketId: "test-socket-id-5",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
     ];
 
     const result = removePlayerKeepListOwned(ps)(p);
 
     const expected: Player[] = [
-      { name: "Test Player 1", socketId: "test-socket-id-1", owner: false },
-      { name: "Test Player 2", socketId: "test-socket-id-2", owner: true },
-      { name: "Test Player 3", socketId: "test-socket-id-3", owner: false },
-      { name: "Test Player 5", socketId: "test-socket-id-5", owner: false },
+      {
+        name: "Test Player 1",
+        socketId: "test-socket-id-1",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 2",
+        socketId: "test-socket-id-2",
+        owner: true,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 3",
+        socketId: "test-socket-id-3",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 5",
+        socketId: "test-socket-id-5",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
     ];
 
     expect(result).toEqual(expected);
@@ -33,23 +83,73 @@ describe("removePlayerKeepListOwned", () => {
       name: "Test Player 2",
       socketId: "test-socket-id-2",
       owner: true,
+      score: 0,
+      guessedWord: false,
     };
 
     const ps: Player[] = [
-      { name: "Test Player 1", socketId: "test-socket-id-1", owner: false },
+      {
+        name: "Test Player 1",
+        socketId: "test-socket-id-1",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
       p,
-      { name: "Test Player 3", socketId: "test-socket-id-3", owner: false },
-      { name: "Test Player 4", socketId: "test-socket-id-4", owner: false },
-      { name: "Test Player 5", socketId: "test-socket-id-5", owner: false },
+      {
+        name: "Test Player 3",
+        socketId: "test-socket-id-3",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 4",
+        socketId: "test-socket-id-4",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 5",
+        socketId: "test-socket-id-5",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
     ];
 
     const result = removePlayerKeepListOwned(ps)(p);
 
     const expected: Player[] = [
-      { name: "Test Player 1", socketId: "test-socket-id-1", owner: true },
-      { name: "Test Player 3", socketId: "test-socket-id-3", owner: false },
-      { name: "Test Player 4", socketId: "test-socket-id-4", owner: false },
-      { name: "Test Player 5", socketId: "test-socket-id-5", owner: false },
+      {
+        name: "Test Player 1",
+        socketId: "test-socket-id-1",
+        owner: true,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 3",
+        socketId: "test-socket-id-3",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 4",
+        socketId: "test-socket-id-4",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
+      {
+        name: "Test Player 5",
+        socketId: "test-socket-id-5",
+        owner: false,
+        score: 0,
+        guessedWord: false,
+      },
     ];
 
     expect(result).toEqual(expected);
@@ -58,6 +158,8 @@ describe("removePlayerKeepListOwned", () => {
   it("returns an empty list when the last player is removed", () => {
     const p: Player = {
       name: "Test Player 1",
+      score: 0,
+      guessedWord: false,
       socketId: "test-socket-id-1",
       owner: true,
     };
